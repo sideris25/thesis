3b62148ad0be2a88f8aab4cb859488df755054e0
sideris25 <sideris2510@gmail.com>
2026-03-19 23:01:58 +0200

    new script, new plots, ////to be debugged

--- train_nn.py
+++ train_nn.py
@@ -12,8 +12,6 @@ X = np.load('dataset_X.npy')
 Y = np.load('dataset_Y.npy')
 
 
-#check cuda
-
 
 #80-20 train-test split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
@@ -26,10 +24,14 @@ Y_test_t = torch.tensor(Y_test, dtype=torch.float32)
 #NN architecture
 input_size = X_train.shape[1]
 output_size = Y_train.shape[1]  
-
+ 
 class InversionNN(nn.Module):
     def __init__(self, input_size, output_size):
         super(InversionNN, self).__init__()
+        '''
+        self.net is a sequential container that defines the architecture of the neural network. It consists of multiple layers, 
+        including linear layers and activation functions (ReLU in this case). The architecture is as follows: 
+          '''
         self.net = nn.Sequential(
             nn.Linear(input_size, 256),
             nn.ReLU(),
